b5a8380f7d6d6bc4d1fdeaae941f64277763e9b7
Alexandre Ledun <alexandre.ledun.etu@univ-lille.fr>
2023-12-26 18:25:31 +0100

    Init project (dependencies, context-loading test)

---
+++ src/main/java/com/tsnt/TasksStackNTrackApplication.java
@@ -0,0 +1,13 @@
+package com.tsnt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TasksStackNTrackApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(TasksStackNTrackApplication.class, args);
+  }
+  
+}
---
+++ src/test/java/com/tsnt/ApplicationTest.java
@@ -0,0 +1,14 @@
+package com.tsnt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = TasksStackNTrackApplication.class)
+public class ApplicationTest {
+  
+  @Test
+  public void contextLoads() {
+    // Tests that the application context loads successfully
+  }
+  
+}
