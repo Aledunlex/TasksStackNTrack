564f3ca5d0d66fd4fbd0ceb7f0e219833bef4430
Aledunlex <alexandre.ledun.etu@univ-lille.fr>
2024-01-11 12:39:12 +0100

    Front-end: inclusion of bootstrap dependency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import HomePage from './pages/HomePage';
 
