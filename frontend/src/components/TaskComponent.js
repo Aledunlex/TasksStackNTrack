80db89fcee1266a2d7a9b7fd2c22492da2dc99e9
Aledunlex <alexandre.ledun.etu@univ-lille.fr>
2024-01-11 14:05:59 +0100

    Front-end: Adding some functionality to task edition & applying some styles to the TaskComponent

--- frontend/src/components/TaskComponent.js
+++ frontend/src/components/TaskComponent.js
@@ -1,8 +1,14 @@
 import {useState} from "react";
+import {toast} from "react-toastify";
 
-const DELETE_TASK_STR = "Supprimer la tâche";
+const DELETE_TASK_STR = "X";
 const APPLY_CHANGES_STR = "Appliquer les modifications";
-const DELETE_TASK_PROP_STR = "Retirer la propriété";
+const DELETE_TASK_PROP_STR = "X";
+const PROPERTY_DELETE_WARNING_STR = "La propriété sera supprimée lors de l'application des modifications";
+const CANCEL_UNSAVED_CHANGES_STR = "Annuler les modifications non sauvegardées";
+const UNSAVED_CHANGES_CANCELED_STR = "Modifications non sauvegardées annulées";
+const NOTHING_TO_CANCEL_STR = "Aucune modification à annuler";
+const ADD_NEW_PROPERTY_STR = "+";
 
 /**
  * React component to display a task and its properties
@@ -12,6 +18,8 @@ const DELETE_TASK_PROP_STR = "Retirer la propriété";
  * @returns {JSX.Element} the React component to display
  */
 const TaskComponent = ({ task, onDelete, onUpdate }) => {
+
+    const originalTask = task;
 
     const [modifiedTask, setModifiedTask] = useState(task);
 
@@ -27,24 +35,67 @@ const TaskComponent = ({ task, onDelete, onUpdate }) => {
 
     const handleRemoveProperty = (propertyId) => {
         const updatedProperties = modifiedTask.taskProperties.filter(prop => prop.id !== propertyId);
+        toast.warn(PROPERTY_DELETE_WARNING_STR);
         setModifiedTask({ ...modifiedTask, taskProperties: updatedProperties });
     };
 
-    const handleUpdateTask = () => {
-        onUpdate(modifiedTask);
+    const handleCancelUnsavedChanges = () => {
+        const modifiedTaskProperties = modifiedTask.taskProperties.map(taskProperty => {
+            return { propertyName: taskProperty.propertyValue.property.name, propertyValue: taskProperty.propertyValue.value };
+        });
+        const originalTaskProperties = originalTask.taskProperties.map(taskProperty => {
+            return { propertyName: taskProperty.propertyValue.property.name, propertyValue: taskProperty.propertyValue.value };
+        });
+        if (modifiedTask.title === originalTask.title
+            && modifiedTask.description === originalTask.description
+            && JSON.stringify(modifiedTaskProperties) === JSON.stringify(originalTaskProperties)) {
+            toast.info(NOTHING_TO_CANCEL_STR);
+            return;
+        }
+        toast.info(UNSAVED_CHANGES_CANCELED_STR);
+        console.log("originalTask", originalTask);
+        setModifiedTask(originalTask);
+    }
+
+    const handleAddProperty = () => {
+        const newProperty = {
+            id: Date.now(),  // Will be replaced by the backend
+            propertyValue: {
+                value: 'Valeur',
+                property: {
+                    name: 'Nouvelle Propriété'
+                }
+            }
+        };
+
+        setModifiedTask({
+            ...modifiedTask,
+            taskProperties: [...modifiedTask.taskProperties, newProperty]
+        });
     };
 
     return (
-        <div key={task.id}>
-            <button onClick={() => onDelete(task.id)}>{DELETE_TASK_STR}</button>
-            <button onClick={handleUpdateTask}>{APPLY_CHANGES_STR}</button>
-            <h2>{task.title}</h2>
-            <p>{task.description}</p>
-            {task.taskProperties && task.taskProperties.map(taskProperty =>
-                <TaskPropertyComponent key={taskProperty.id} taskProperty={taskProperty}
-                                       onPropertyChange={handlePropertyChange}
-                                       onRemoveProperty={handleRemoveProperty} />
-            )}
+        <div key={modifiedTask.id} className="card mb-3">
+            <div className="card-body">
+                <div className="d-flex justify-content-between align-items-center">
+                    <h2 className="card-title mb-0">{modifiedTask.title}</h2>
+                    <div>
+                        <button onClick={() => handleCancelUnsavedChanges()}
+                                className="btn btn-outline-secondary me-2">{CANCEL_UNSAVED_CHANGES_STR}</button>
+                        <button onClick={() => onUpdate(modifiedTask)}
+                                className="btn btn-primary me-2">{APPLY_CHANGES_STR}</button>
+                        <button onClick={() => onDelete(modifiedTask.id)}
+                                className="btn btn-danger">{DELETE_TASK_STR}</button>
+                    </div>
+                </div>
+                <p className="card-text mt-2">{modifiedTask.description}</p>
+                {modifiedTask.taskProperties && modifiedTask.taskProperties.map(taskProperty =>
+                    <TaskPropertyComponent key={taskProperty.id} taskProperty={taskProperty}
+                                           onPropertyChange={handlePropertyChange}
+                                           onRemoveProperty={handleRemoveProperty}/>
+                )}
+                <button onClick={handleAddProperty} className="btn btn-primary">{ADD_NEW_PROPERTY_STR}</button>
+            </div>
         </div>
     );
 
@@ -60,14 +111,15 @@ const TaskComponent = ({ task, onDelete, onUpdate }) => {
 const TaskPropertyComponent = ({ taskProperty, onPropertyChange, onRemoveProperty }) => {
 
     return (
-        <div>
+        <div className="d-flex justify-content-between align-items-center mb-3">
             <PropertyValueComponent
+                key={taskProperty.propertyValue.id}
                 propertyValue={taskProperty.propertyValue}
                 onValueChange={(newValue) => {
                     onPropertyChange(taskProperty.id, newValue);
                 }}
             />
-            <button onClick={() => onRemoveProperty(taskProperty.id)}>{DELETE_TASK_PROP_STR}</button>
+            <button onClick={() => onRemoveProperty(taskProperty.id)} className="btn btn-outline-secondary ms-2">{DELETE_TASK_PROP_STR}</button>
         </div>
     );
 
@@ -81,15 +133,15 @@ const TaskPropertyComponent = ({ taskProperty, onPropertyChange, onRemovePropert
  */
 const PropertyValueComponent = ({ propertyValue, onValueChange }) => {
 
-    const handleInputChange = (propertyValue, newValue) => {
-        propertyValue.value = newValue;
-        onValueChange(newValue);
-    };
-
     return (
-        <div>
-            <label>{propertyValue.property.name} :</label>
-            <input onChange={e => handleInputChange(propertyValue, e.target.value)} type="text" value={propertyValue.value} />
+        <div className="input-group">
+            <label className="input-group-text">{propertyValue.property.name} :</label>
+            <input
+                onChange={e => onValueChange(e.target.value)}
+                type="text"
+                value={propertyValue.value}
+                className="form-control"
+            />
         </div>
     );
 
